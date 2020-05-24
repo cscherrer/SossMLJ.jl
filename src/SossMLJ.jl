ec5b93c43741ccf348dd6e8953be414a39a2ce76
Chad Scherrer <chad.scherrer@gmail.com>
2020-05-24 10:32:55 -0700

    Type parameterization

--- src/SossMLJ.jl
+++ src/SossMLJ.jl
@@ -6,6 +6,7 @@ const MMI = MLJModelInterface
 using Soss
 import Soss: Model, @model, predictive, Univariate, Continuous, dynamicHMC
 using Distributions
+import Base
 
 # Just a placeholder
 m0 = @model X begin
@@ -19,29 +20,49 @@ end
 # TODO: Add an way to specify which variable is observed, and which is predicted
 # e.g. even if we observe `y`, we might sometimes prefer to predict yhat
 
-@mlj_model mutable struct SossModel <: MMI.Probabilistic
-    model :: Model = m0
+# @mlj_model mutable struct SossMLJModel <: MMI.Probabilistic
+#     model :: Model = m0
+# end
+
+
+export SossMLJModel
+
+mutable struct SossMLJModel{A,B,M} <: MLJModelInterface.Probabilistic
+    model::Model{A,B,M}
 end
 
-struct SossPredictor <: Distributions.Sampleable{Univariate,Continuous}
-    model
+function SossMLJModel(; model = m0)
+    model = SossMLJModel(model)
+    message = MLJModelInterface.clean!(model)
+    return model
+end
+
+
+function MLJModelInterface.clean!(model::SossMLJModel)
+    warning = ""
+    return warning
+end
+
+struct SossPredictor{A,B,M} <: Distributions.Sampleable{Univariate,Continuous}
+    model :: Model{A,B,M}
     post
     xrow
 end
 
-function rand(sp::SossPredictor)
+
+function Base.rand(sp::SossPredictor{A,B,M}) where {A,B,M}
     par = rand(sp.post)
     X = reshape(sp.xrow, (1,:))
     args = merge(par, (X=X,))
-    rand(sp.model(args)).y[1]
+    return rand(sp.model(args)).y[1]
 end
 
-function MMI.fit(sm::SossModel, verbosity::Int, X, y, w=nothing)
+function MMI.fit(sm::SossMLJModel, verbosity::Int, X, y, w=nothing)
     # construct the model
     
     jd = sm.model(X=X)
 
-    # TODO: Specify inference method in SossModel
+    # TODO: Specify inference method in SossMLJModel
     post = dynamicHMC(jd, (y=y,))
 
     # TODO: Allow w to be included
@@ -54,7 +75,7 @@ function MMI.fit(sm::SossModel, verbosity::Int, X, y, w=nothing)
     ((model=m, post=post), cache, report)
 end
 
-function MMI.predict(sm::SossModel, fitresult, Xnew)
+function MMI.predict(sm::SossMLJModel, fitresult, Xnew)
     m = fitresult.model
     post = fitresult.post
     
