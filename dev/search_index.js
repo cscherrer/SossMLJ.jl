8088521763c67997c03e7acc9532e67165ffb16b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-16 19:57:03 +0000

    build based on 7005c54

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SossMLJ.jl","page":"Home","title":"SossMLJ.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"SossMLJ integrates the Soss probabilistic programming library into the MLJ machine learning framework.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SossMLJ]","category":"page"}]
+}
