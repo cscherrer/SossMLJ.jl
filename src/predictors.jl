761a60965ea1753ac0874349366d45d175661d06
Chad Scherrer <chad.scherrer@gmail.com>
2020-08-24 14:02:33 -0700

    bugfixes

--- src/predictors.jl
+++ src/predictors.jl
@@ -8,14 +8,15 @@ function Base.rand(sp::SossMLJPredictor{M};
     return getproperty(rand(sp.pred(args)), response)
 end
 
-function Distributions.logpdf(sp::SossMLJPredictor{M}, y) where {M}
+function Distributions.logpdf(sp::SossMLJPredictor{M}, y;
+        response = sp.model.response) where {M}
     # Get all the distribution mixture components
     dists = Base.Generator(sp.post) do pars
         args = merge(sp.args, pars)
         sp.pred(args)
     end
 
-    y_namedtuple = namedtuple(sm.response)(y)
+    y_namedtuple = namedtuple(response)(tuple(y))
     # Evaluate logpdf(d,x) on each component d
     logvals = Base.Generator((d -> Distributions.logpdf(d, y_namedtuple)) ∘ dists.f, dists.iter)
     n = length(sp.post)
