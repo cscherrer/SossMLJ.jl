0f9cda3e57b9d816af76b6c34fe85d6c5d2a4161
Chad Scherrer <chad.scherrer@gmail.com>
2021-03-29 15:37:02 -0700

    getting close I think

--- src/particles.jl
+++ src/particles.jl
@@ -20,45 +20,41 @@ function predict_particles(response::Symbol)
         if :response in keys(kwargs)
             throw(ArgumentError("you cannot provide the `response` keyword argument to this method"))
         end
-        return predict_particles(varargs...; response = response, kwargs...)
+        return predict_particles(varargs...; response=response, kwargs...)
     end
     return _predict_particles
 end
 
 function predict_particles(original_predictor::SossMLJPredictor,
                            Xnew;
-                           response = original_predictor.model.response_predict)
+                           response=original_predictor.model.response_predict)
     original_args = original_predictor.args
     original_sm  = original_predictor.model
     original_post = original_predictor.post
     original_model = original_sm.model
     original_transform = original_sm.transform
-    original_pars = Soss.particles(original_post)
-    if response in keys(original_pars)
-        return original_pars
+    if response in keys(original_post[1])
+        return Soss.particles(getproperty.(original_post, response))
     end
     new_model = Soss.before(
         original_model,
         response;
-        inclusive = true,
-        strict = false,
-    )
+        inclusive=true,
+        strict=false,
+        )
     new_pred = Soss.predictive(
-        new_model,
-        keys(original_post[1])...,
-    )
-    new_dist = new_pred(merge(original_args, original_transform(Xnew), original_pars))
-    return Soss.particles(new_dist)
+            new_model,
+            keys(original_post[1])...,
+            )
+    # TODO: Make this more efficient
+    new_dist = [new_pred(merge(original_args, original_transform(Xnew), p)) for p in original_post]
+    return Soss.particles(getproperty.(rand.(new_dist), response))
 end
 
 function predict_particles(sm::SossMLJModel,
                            fitresult,
                            Xnew;
-                           response = sm.response_predict)
+                           response=sm.response_predict)
     predictor_joint = MLJModelInterface.predict_joint(sm, fitresult, Xnew)
-    result_particles = getproperty(
-        predict_particles(predictor_joint, Xnew; response = response),
-        response,
-    )
-    return result_particles
+    return predict_particles(predictor_joint, Xnew; response=response)
 end
